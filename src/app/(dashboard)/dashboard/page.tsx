4f7223fa629fa3b24f65e4e5fe3956bc02c5a5cd
Marco Gonzalez <magp2606@gmail.com>
2023-05-06 14:32:37 -0500

    fixed last messages bug

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -38,48 +38,52 @@ const Dashboard = async () => {
       {friendsWithLastMessage.length === 0 ? (
         <p className='text-zinc-500 text-sm'>Nothing to show here</p>
       ) : (
-        friendsWithLastMessage.filter(friend => friend !== undefined).map((friend) => {
-          return (
-            <div
-              key={friend?.id}
-              className='relative bg-zinc-50 border border-zinc-200 p-3 rounded-md'>
-              <div className='absolute right-4 inset-y-0 flex items-center'>
-                <ChevronRight className='h-7 w-7 text-zinc-400' />
-              </div>
-              <Link
-                href={`/dashboard/chat/${chatHrefConstructor(
-                  session.user.id,
-                  friend?.id
-                )}`}
-                className='relative sm:flex'>
-                <div className='mb-4 flex-shrink-0 sm:mb-0 sm:mr-4 items-center'>
-                  <div className='relative h-12 w-12'>
-                    <Image
-                      referrerPolicy='no-referrer'
-                      className='rounded-full'
-                      alt={`${friend?.name} profile picture`}
-                      fill
-                      src={friend?.image}
-                    />
+        friendsWithLastMessage
+          .filter((friend) => friend !== undefined)
+          .map((friend) => {
+            return (
+              <div
+                key={friend?.id}
+                className='relative bg-zinc-50 border border-zinc-200 p-3 rounded-md'>
+                <div className='absolute right-4 inset-y-0 flex items-center'>
+                  <ChevronRight className='h-7 w-7 text-zinc-400' />
+                </div>
+                <Link
+                  href={`/dashboard/chat/${chatHrefConstructor(
+                    session.user.id,
+                    friend?.id
+                  )}`}
+                  className='relative sm:flex'>
+                  <div className='mb-4 flex-shrink-0 sm:mb-0 sm:mr-4 items-center'>
+                    <div className='relative h-12 w-12'>
+                      {friend?.image && (
+                        <Image
+                          referrerPolicy='no-referrer'
+                          className='rounded-full'
+                          alt={`${friend?.name} profile picture`}
+                          fill
+                          src={friend?.image}
+                        />
+                      )}
+                    </div>
                   </div>
-                </div>
-                <div>
-                  <h4 className='text-lg font-semibold'>{friend?.name}</h4>
-                  <p className='mt-1 max-w-md'>
-                    <span className='text-zinc-400'>
-                      {friend?.lastMessage.senderId === session.user.id
-                        ? 'You: '
-                        : ''}
-                    </span>
-                    <span className='text-zinc-700 font-semibold'>
-                      {friend?.lastMessage.text}
-                    </span>
-                  </p>
-                </div>
-              </Link>
-            </div>
-          )
-        })
+                  <div>
+                    <h4 className='text-lg font-semibold'>{friend?.name}</h4>
+                    <p className='mt-1 max-w-md'>
+                      <span className='text-zinc-400'>
+                        {friend?.lastMessage.senderId === session.user.id
+                          ? 'You: '
+                          : ''}
+                      </span>
+                      <span className='text-zinc-700 font-semibold'>
+                        {friend?.lastMessage.text}
+                      </span>
+                    </p>
+                  </div>
+                </Link>
+              </div>
+            )
+          })
       )}
     </div>
   )
